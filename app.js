66cb432c8789915871f1d4e44af5213c0bb9d3aa
Bunma93 <bunma93@gmail.com>
2024-07-04 16:50:50 +0700

    Js-obj17

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // write code here
+const permission1 = { canView: false, canDelete: false };
+const permission2 = { canUpdate: true, canCreate: true };
+const permission3 = { canCreate: false, canDelete: true };
+const permission4 = { canView: true };
+
+Object.assign(permission1, permission2, permission3, permission4);
+console.log(permission1);
